390c645523f05f87485fcccf90e1d276deb54b0d
peilun-he <allen.he93.ad@gmail.com>
2023-06-08 17:31:32 +1000

    Update my_ui.R

--- R/my_ui.R
+++ R/my_ui.R
@@ -2,7 +2,7 @@ ver <<- "2.1"
 
 my_ui <- fluidPage(
   theme = shinytheme("cyborg"), 
-  navbarPage(title = paste("PDSim (v", ver, ")"), 
+  navbarPage(title = paste("PDSim (v", ver, ")", sep = ""), 
              draw_welcome(), # draw welcome page
              draw_app(), # draw app page
              draw_user_guide(), # draw user guide page
