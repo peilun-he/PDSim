d7a69d6960b539ca899b0352daf40fef198d8e05
peilun-he <63130900+peilun-he@users.noreply.github.com>
2024-04-27 01:35:35 +1000

    Add files via upload

---
+++ docker_installation.R
@@ -0,0 +1,10 @@
+install.packages(c('DT', 
+                   'ggplot2', 
+                   'lubridate', 
+                   'plotly', 
+                   'scales', 
+                   'shiny', 
+                   'shinythemes', 
+                   'tidyr'))
+
+install.packages('/home/PDSim', repos = NULL, type="source")
